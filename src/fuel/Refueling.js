5c8b0838c0ded41e57750f47450d3800f7306398
Dominik <d.beczyczko@gmail.com>
2020-01-26 15:37:52 +0100

    initial commit

---
+++ src/fuel/Refueling.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './refueling.scss';
+
+const Refueling = ({refueling}) => {
+    return (
+        <div className="refueling">
+            <p className="amount">{refueling.amount} L</p>
+            <p className="date">{refueling.date}</p>
+            {/*todo human friendly date*/}
+        </div>
+    );
+};
+
+export default Refueling;
